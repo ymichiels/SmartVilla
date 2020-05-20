fb25ee88b79507983999dbd95e597f36ca8f02bb
JCeb <johann.cebollado@etud.univ-pau.fr>
2020-05-20 16:00:24 +0200

    source platform.io

--- embedded/src/ESP8266.cpp
+++ embedded/src/ESP8266.cpp
@@ -1,46 +1,43 @@
-// from https://github.com/esp8266/Arduino/blob/3e7b4b8e0cf4e1f7ad48104abfc42723b5e4f9be/variants/nodemcu/pins_arduino.h
-const uint8_t D0   = 16;
-const uint8_t D1   = 5;
-const uint8_t D2   = 4;
-const uint8_t D3   = 0;
-const uint8_t D4   = 2;
-const uint8_t D5   = 14;
-const uint8_t D6   = 12;
-const uint8_t D7   = 13;
-const uint8_t D8   = 15;
-const uint8_t D9   = 3;
-const uint8_t D10  = 1;
+//-- end test
+//#include "data.h"
+
+// define WIFI_SSID, WIFI_PWD
+//        MQTT_IP, MQTT_PORT, MQTT_NAME, MQTT_PWD
+#include "config_wifi.h"
 
 //#define WIFI_USED
-#define WIFI_SSID "REDACTED"
-#define WIFI_PWD "REDACTED"
-
 #define SEND_INVALID 1
 
-#define TOPIC_PREFIX "/smart-villa/etage1/"
+#define TOPIC_PREFIX "smart-villa/etage1/esp8266/"
 
 #define LIGHT_SENS
-#define LIGHT_TOPIC "lum"
+#define TOPIC_LIGHT "lum"
 #define LIGHT_PIN A0
 
 #define DHT_SENS
-#define DHT_HUMIDITE_TOPIC "hum"
-#define DHT_TEMPERATURE_TOPIC "temp"
-#define DHT_PIN D0
+#define TOPIC_HUMIDITE "hum"
+#define TOPIC_TEMPERATURE "temp"
+#define DHT_PIN D4
 
 #define MOTION_SENS
-#define MOTION_TOPIC "mouv"
+#define TOPIC_MOTION "mouv"
 #define PIR_PIN D2
 
 #define DIST_SENS
-#define DIST_TOPIC "dist"
+#define TOPIC_DIST "dist"
 #define TRIG_PIN D5
 #define ECHO_PIN D6
 
+// CONFIG - DONE
+
+//#define WIFI_USED
 #ifdef WIFI_USED
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+WiFiClient client;
+PubSubClient psc{MQTT_IP, MQTT_PORT, client};
 #endif
+
 
 #ifdef DHT_SENS
 #include <DHT.h>
@@ -51,6 +48,21 @@ DHT dht(DHT_PIN, DHT11);
 // boolean indicating wether something was detected last cycle
 bool motionLastState;
 #endif
+
+
+template<class T>
+void logValue(const char category[], const T& value) {
+  String raw{value};
+#ifdef WIFI_USED
+  String topic{TOPIC_PREFIX};
+  topic += category;
+  psc.publish(topic.c_str(), raw.c_str());
+#else
+  Serial.print(category);
+  Serial.print(" => ");
+  Serial.println(raw);
+#endif
+}
 
 
 void setup() {
@@ -69,13 +81,13 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // cc "https://iotdesignpro.com/projects/how-to-connect-esp8266-with-mqtt"
-  while(!client.connected()) {
+  while(!psc.connected()) {
     Serial.println("Connection to MQTT...");
-    if (client.connect("ESP8266Client", "", "")) {
-       Serial.println("Connected");
+    if (psc.connect("ESP8266Client", MQTT_NAME, MQTT_PWD)) {
+      Serial.println("Connected");
     } else {
       Serial.print("failed with state");
-      Serial.println(client.state());
+      Serial.println(psc.state());
       delay(2000);
     }
   }
@@ -98,50 +110,43 @@ void setup() {
 
 void loop()
 {
-  int debut = millis();
+  unsigned long start = millis();
+  digitalWrite(D0, digitalRead(D0));
 #ifdef LIGHT_SENS
   {
     int lum = analogRead(LIGHT_PIN);
-  #ifdef WIFI_USED
-    Serial.println("light: " + String(lum));
-  #else
-    
-  #endif
+    logValue(TOPIC_LIGHT, lum);
   }
 #endif
+
 #ifdef DHT_SENS
   {
     float temp = dht.readTemperature();
     float hum = dht.readHumidity();
     if(!isnan(temp)){
-//      payload.add("temperature", String(temp));
+      logValue(TOPIC_TEMPERATURE, temp);
     } else {
       #if SEND_INVALID
-//      payload.add("temperature", "NaN");
+      logValue(TOPIC_TEMPERATURE, "NaN");
       #endif
     }
     if(!isnan(hum)){
-//      payload.add("humidity", String(hum));
+      logValue(TOPIC_HUMIDITE, hum);
     } else {
       #if SEND_INVALID
-//      payload.add("humidity", "NaN");
+      logValue(TOPIC_HUMIDITE, "NaN");
       #endif
     }
   }
 #endif
+
 #ifdef MOTION_SENS
   {
     bool detect = digitalRead(PIR_PIN) == HIGH;
-    if(detect == motionLastState){
-//      payload.add("pir_changed", "false");
-    } else {
-//      payload.add("pir_changed", "true");
-    }
-
-//    payload.add("pir_detect ", detect ? "true" : "false");
-    motionLastState = detect;
+    logValue(TOPIC_MOTION, detect);
   }
 #endif
+
 #ifdef DIST_SENS
   {
     float duration, distanceCm;
@@ -158,23 +163,17 @@ void loop()
    
     if (distanceCm <= 0){
       #if SEND_INVALID
-//      payload.add("distance", "Infinity");
+      logValue(TOPIC_DIST, "Infinity");
       #endif
     } else {
-//      payload.add("distance", String(distanceCm));
+      logValue(TOPIC_DIST, distanceCm);
     }
   }
 #endif
+
   Serial.print("payload computed in ");
-  //Serial.print(payload.currentTime());
+  Serial.print(millis() - start);
   Serial.println("ms");
-/*  for(Data& item : payload){
-    Serial.print(item.category);
-    Serial.print(": ");
-    Serial.print(item.reprValue);
-    Serial.println();
-  }*/
   Serial.println();
-  //payload.reset();
   delay(1000);
 }
