5cd9af810e0a89d73ea17dfe8aa87bd288341fd1
JCeb <johann.cebollado@etud.univ-pau.fr>
2020-05-20 18:34:08 +0200

    added .gitignore and config_wifi.h

---
+++ embedded/src/config_wifi.h
@@ -0,0 +1,30 @@
+/*This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
+#ifndef CONFIG_WIFI_HEADER
+#define CONFIG_WIFI_HEADER
+
+// nom du réseaux wifi
+#define WIFI_SSID ""
+// mot de passe du wifi
+#define WIFI_PWD ""
+
+// ip du brocker MQTT
+#define MQTT_IP IPAddress(127, 0, 0, 1)
+// port du brocker MQTT
+#define MQTT_PORT 1883
+
+// nom d'utilisateur MQTT
+#define MQTT_NAME ""
+// mot de passe de l'utilisateur mqtt
+#define MQTT_PWD ""
+
+#endif // CONFIG_WIFI_HEADER
